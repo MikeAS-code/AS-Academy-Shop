2ea3e2b2ccd5cbb88407a62fbb28871ad3faa7df
MauroSezella <maurosezella@gmail.com>
2026-09-09 12:06:54 -0300

    generamos la carga automatica de reviews

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from web_driver import WebDriver
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from lxml import html
 import config
+import random
 
 def login(navegador, username, password):
 
@@ -25,6 +26,8 @@ def login(navegador, username, password):
 
 
 def get_products(navegador):
+
+    products = []
     
     button_products = WebDriverWait(navegador, 10).until(
         EC.element_to_be_clickable(
@@ -40,9 +43,122 @@ def get_products(navegador):
 
     tree = html.fromstring(html_products)
 
-    productos = tree.xpath('//div[@data-testid="product-card"]')
+    card_products = tree.xpath('//div[@data-testid="product-card"]')
 
-    return productos
+    for card in card_products:
+        try:
+            title = card.xpath('.//h3[@data-testid="product-title"]/text()')[0]
+            if not title:
+                title = " "
+        except Exception as e:
+            title = " "
+
+        try:
+            price = card.xpath('.//span[@data-testid="product-price"]/text()')[0]
+            if not price:
+                price = 0
+        except Exception as e:
+            price = 0
+
+        try:
+            category = card.xpath('.//span[@data-testid="product-category"]/text()')[0]
+            if not category:
+                category = " "
+        except Exception as e:
+            category = " "
+
+        try:
+            description = card.xpath('.//p[@data-testid="product-description"]/text()')[0]
+            if not description:
+                description = " "
+        except Exception as e:
+            description = " "
+
+        try:
+            availability = card.xpath('.//span[@data-testid="product-status"]/text()')[0]
+            if not availability:
+                availability = " "
+        except Exception as e:
+            availability = " "
+
+        try:
+            image = card.xpath('.//img[@data-testid="product-card-image"]/@src')[0]
+            if not image:
+                image = " "
+        except Exception as e:
+            image = " "
+
+        products.append({
+            'title': title,
+            'price': price,
+            'category': category,
+            'description': description,
+            'availability': availability,
+            'image': image
+        })
+    return products
+
+def cargar_reviews(navegador):
+
+    # Cantidad de productos de la página actual
+    botones = navegador.find_elements(
+        By.XPATH,
+        '//button[@data-testid="product-detail-button"]'
+    )
+
+    total = len(botones)
+
+    print(f"Productos encontrados: {total}")
+
+    for i in range(total):
+
+        print(f"Procesando producto {i + 1}/{total}")
+
+        boton_ver_detalle = botones[i]
+
+          # Llevar el botón al centro de la pantalla
+        navegador.execute_script(
+            "arguments[0].scrollIntoView({block: 'center'});",
+            boton_ver_detalle
+        )
+
+        boton_ver_detalle.click()
+
+        time.sleep(4)
+
+        name_product = navegador.find_element(
+            By.XPATH,
+            '//h2'
+        ).text
+
+        price_product = navegador.find_element(By.XPATH, '//h2/following-sibling::p[1]').text.replace("$", "")
+
+        texto_review = f"La verdad que el producto {name_product} cumple con mis expectativas, su precio {price_product} totalmente accesible. Recomiendo este producto."
+
+        navegador.find_element(By.XPATH, '//input[@data-testid="comment-name"]').send_keys(config.nombres[random.randint(0, len(config.nombres) - 1)])
+
+        rating = random.choice(["3", "4", "5"])
+
+        select = Select(
+                navegador.find_element(
+                By.CSS_SELECTOR,
+                'select[data-testid="comment-rating"]'
+            )
+        )
+
+        select.select_by_value(rating)
+
+        navegador.find_element(By.XPATH, '//textarea[@data-testid="comment-text"]').send_keys(texto_review)
+
+        time.sleep(5)
+
+        boton_cerrar_modal = navegador.find_element(
+            By.XPATH,
+            '//button[@aria-label="Cerrar modal"]'
+        )
+        boton_cerrar_modal.click()
+        time.sleep(2)
+
 
 
 if __name__ == "__main__":
@@ -56,7 +172,10 @@ if __name__ == "__main__":
 
     login(navegador.get_driver(), config.USERNAME, config.PASSWORD)
 
-    productos = get_products(navegador.get_driver())
-    print(f"Total products found: {len(productos)}")
+    products = get_products(navegador.get_driver())
+    ##print(f"Total products found: {len(products)}")
+    ##print("Products:", products)
+    cargar_reviews(navegador.get_driver())
+
 
     print("End crawler")
